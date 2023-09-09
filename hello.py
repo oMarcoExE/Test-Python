dc487fcef115e393c9acac6496039775fc55fa49
Marco Antonio <marco23a@hotmail.com>
2023-09-09 10:51:03 -0300

    Meu arquivo Hello World

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+nome = "Marco"
+profissao = "Estudante"
+
+print (f"\nPrimeiro código em python de {nome}")
+print ("Eu sou ", profissao, " e decidi começar python \n")
+
+resposta = input("Você está pronto para vir comigo (S/N)")
