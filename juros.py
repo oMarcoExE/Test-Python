058508a322dad1000c7b5bf8734aeef885368f31
Marco Antonio <marco23a@hotmail.com>
2023-09-09 14:25:06 -0300

    att juros

--- juros.py
+++ juros.py
@@ -6,6 +6,6 @@ valorPRIN = float (input("Informe o valor principal: ") )
 jurosANUAL = float(input("Informe a taxa de juros anual: "))
 tempo = int (input("Informe o período de tempo em anos: "))
 
-montante = valorPRIN + (valorPRIN * jurosANUAL * tempo)
+montante = valorPRIN + (valorPRIN * (jurosANUAL/100) * tempo)
 
 print(f"\nSeu montante é de {montante}")
