c7f9e082aba827c1208ca8c5040de8f74b7e6556
Marco Antonio <marco23a@hotmail.com>
2023-09-09 12:04:22 -0300

    arquivos Calculos e IMC

---
+++ IMC.py
@@ -0,0 +1,15 @@
+# Calculo de IMC
+
+### nome, idade, peso, altura // e calcular imc
+
+nome = input("Olá! Qual seu nome?: ")
+idade = input("Qual sua idade? ")
+altura = float(input("Sua altura: "))
+peso = float(input ("Seu peso: "))
+
+IMC = peso / (altura ** 2)
+
+
+print(f"Olá {nome}!")
+print(f"Sua idade é de {idade} anos")
+print(f"Seu IMC é de {IMC}")
---
+++ calculos.py
@@ -0,0 +1,10 @@
+# Arquivo relacionado a testes de cáculos
+
+nmr1 = 12
+nmr2 = 13
+
+print ( nmr1 + nmr2)
+
+res = nmr1 + nmr2
+print ( "O Resultado é: ", res)
+print (f"O resultado é:  {res}")
