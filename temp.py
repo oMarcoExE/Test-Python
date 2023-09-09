0322e443a02b2ad241951218946c1ebbc769fe05
Marco Antonio <marco23a@hotmail.com>
2023-09-09 14:15:19 -0300

    att Temp

--- temp.py
+++ temp.py
@@ -5,5 +5,7 @@ print("Conversão de temperatura\n")
 C = float(input("Digite a Temperatura em Celsius: "))
 
 F = (1.8*C) + 32
+K = C + 273
 
 print("Sua temperatura em farenheit é: ", F)
+print("Sua temperatura em kelvin é: ", K)
