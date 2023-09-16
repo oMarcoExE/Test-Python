0abba27fc9ae0391f780fc47c3b474e99244c632
Marco Antonio <marco23a@hotmail.com>
2023-09-16 17:19:18 -0300

    commir correção

--- lojaroupa.py
+++ lojaroupa.py
@@ -11,18 +11,18 @@ if pagamento == 'A Vista':
         totalcompra = totalcompra - (totalcompra * 20/100)
 
 if pagamento == 'A Prazo':
-    parcelas = int(input("Informe a quantidade de parcelas: "))
+    parcelas = float(input("Informe a quantidade de parcelas: "))
     if totalcompra < 800 and parcelas >= 6:
         print("Compras abaixo de 800 reais só podem ser parceladas até 5x!")
     elif parcelas <= 10: totalcompra = totalcompra
-    elif parcelas == 11: totalcompra = totalcompra + (totalcompra * 5/100)
-    elif parcelas == 12: totalcompra = totalcompra + (totalcompra * 6.5/100)
-    elif parcelas == 13: totalcompra = totalcompra + (totalcompra * 7/100)
-    elif parcelas == 14: totalcompra = totalcompra + (totalcompra * 9/100)
-    elif parcelas == 15: totalcompra = totalcompra + (totalcompra * 9.5/100)
-    elif parcelas == 16: totalcompra = totalcompra + (totalcompra * 10/100)
-    elif parcelas == 17: totalcompra = totalcompra + (totalcompra * 11.3/100)
-    elif parcelas == 18: totalcompra = totalcompra + (totalcompra * 12/100)
+    elif parcelas == 11: totalcompra = totalcompra + totalcompra * (0.05 * parcelas)
+    elif parcelas == 12: totalcompra = totalcompra + totalcompra * (0.065 * parcelas)
+    elif parcelas == 13: totalcompra = totalcompra + totalcompra * (0.07 * parcelas)
+    elif parcelas == 14: totalcompra = totalcompra + totalcompra * (0.09 * parcelas)
+    elif parcelas == 15: totalcompra = totalcompra + totalcompra * (0.095 * parcelas)
+    elif parcelas == 16: totalcompra = totalcompra + totalcompra * (0.1 * parcelas)
+    elif parcelas == 17: totalcompra = totalcompra + totalcompra * (0.0113 * parcelas)
+    elif parcelas == 18: totalcompra = totalcompra + totalcompra * (0.012 * parcelas)
     else: print("Quantidade não valida")
 
-print(f"Sua compra Deu um total R$ {totalcompra}")
+print(f"Sua compra Deu um total R$ {totalcompra}") 
