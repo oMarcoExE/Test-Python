d183c1de0e666d61a86be83c0915e67b7ab2f22a
Marco Antonio <marco23a@hotmail.com>
2023-09-16 09:18:14 -0300

    commit atividade

--- lojaroupa.py
+++ lojaroupa.py
@@ -13,7 +13,25 @@ if pagamento == 'A Vista':
 
 if pagamento == 'A Prazo':
     parcelas = int(input("Informe a quantidade de parcelas: "))
-    if totalcompra < 800:
-        print("Valor total de compra = ", totalcompra) 
-
-
+    if totalcompra < 800 and parcelas >= 6:
+        print("Compras abaixo de 800 reais só podem ser parceladas até 5x!")
+    elif parcelas <= 10:
+        print("O valor total será de: ", totalcompra)
+    elif parcelas == 11:
+        print("O valor total será de: ", totalcompra + (totalcompra * 5/100))
+    elif parcelas == 12:
+        print("O valor total será de: ", totalcompra + (totalcompra * 6.5/100))
+    elif parcelas == 13:
+        print("O valor total será de: ", totalcompra + (totalcompra * 7/100))
+    elif parcelas == 14:
+        print("O valor total será de: ", totalcompra + (totalcompra * 9/100))
+    elif parcelas == 15:
+        print("O valor total será de: ", totalcompra + (totalcompra * 9.5/100))
+    elif parcelas == 16:
+        print("O valor totalserá de: ", totalcompra + (totalcompra * 10/100))
+    elif parcelas == 17:
+        print("O valor total será de: ", totalcompra + (totalcompra * 11.3/100))
+    elif parcelas == 18:
+        print("O valor total será de: ", totalcompra + (totalcompra * 12/100))
+    else:
+        print("Quantidade não valida")
