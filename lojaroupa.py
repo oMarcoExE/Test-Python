e6ebf0df4012cb3d80f36ec3caf7ee014624f827
Marco Antonio <marco23a@hotmail.com>
2023-09-16 12:54:17 -0300

    commit

--- lojaroupa.py
+++ lojaroupa.py
@@ -4,33 +4,34 @@ pagamento = input("Qual forma de pagamento? (A Vista / A Prazo): ")
 
 if pagamento == 'A Vista':
     if totalcompra < 500:
-        print("O Valor será de: ", totalcompra - (totalcompra * 10/100))
-    elif totalcompra > 500:
-        print("O Valor será de: ", totalcompra - (totalcompra * 15/100))
+        totalcompra = totalcompra - (totalcompra * 10/100)
+    elif totalcompra > 500 and totalcompra < 1000:
+        totalcompra = totalcompra - (totalcompra * 15/100)
     elif totalcompra > 1000:
-        print("O Valor será de: ", totalcompra - (totalcompra * 20/100))
+        totalcompra = totalcompra - (totalcompra * 20/100)
 
 if pagamento == 'A Prazo':
     parcelas = int(input("Informe a quantidade de parcelas: "))
     if totalcompra < 800 and parcelas >= 6:
         print("Compras abaixo de 800 reais só podem ser parceladas até 5x!")
     elif parcelas <= 10:
-        print("O valor total será de: ", totalcompra)
+        totalcompra = totalcompra
     elif parcelas == 11:
-        print("O valor total será de: ", totalcompra + (totalcompra * 5/100))
+        totalcompra = totalcompra + (totalcompra * 5/100)
     elif parcelas == 12:
-        print("O valor total será de: ", totalcompra + (totalcompra * 6.5/100))
+        totalcompra = totalcompra + (totalcompra * 6.5/100)
     elif parcelas == 13:
-        print("O valor total será de: ", totalcompra + (totalcompra * 7/100))
+        totalcompra = totalcompra + (totalcompra * 7/100)
     elif parcelas == 14:
-        print("O valor total será de: ", totalcompra + (totalcompra * 9/100))
+        totalcompra = totalcompra + (totalcompra * 9/100)
     elif parcelas == 15:
-        print("O valor total será de: ", totalcompra + (totalcompra * 9.5/100))
+        totalcompra = totalcompra + (totalcompra * 9.5/100)
     elif parcelas == 16:
-        print("O valor totalserá de: ", totalcompra + (totalcompra * 10/100))
+       totalcompra = totalcompra + (totalcompra * 10/100)
     elif parcelas == 17:
-        print("O valor total será de: ", totalcompra + (totalcompra * 11.3/100))
+        totalcompra = totalcompra + (totalcompra * 11.3/100)
     elif parcelas == 18:
-        print("O valor total será de: ", totalcompra + (totalcompra * 12/100))
+        totalcompra = totalcompra + (totalcompra * 12/100)
     else:
         print("Quantidade não valida")
+print(f"Sua compra Deu um total R$ {totalcompra}")
