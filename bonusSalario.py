d910a7d4f66a2f6f9b63ddf10d1fceee6fdd3c56
Marco Antonio <marco23a@hotmail.com>
2023-09-16 11:47:36 -0300

    bonusSalario commit

--- bonusSalario.py
+++ bonusSalario.py
@@ -4,6 +4,6 @@ sal = float(input("Insira seu Salário: "))
 temp = int(input("Quanto tempo de serviço(em anos): "))
 
 if temp >= 5:
-    print("Você contribuiu por mais de 5 anos \n seu salário bônus salarial é: ", sal + (sal *5/100))
+    print("Você contribuiu por mais de 5 anos \n seu salário bônus salarial é: ", sal * 1.05 )
 else:
     print("Menos de 5 anos contribuindo. Sem bônus Salarial")
